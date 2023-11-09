bfd725d9f16fecff9705cfd39baeb1121137a643
jacksonn97 <jackson.n.97@proton.me>
2023-11-09 19:48:14 +0300

    Fix mistakes

--- src/draw.rs
+++ src/draw.rs
@@ -4,8 +4,6 @@ use std::{
     thread,
     sync::{
         Arc,
-        Mutex,
-        atomic::{ AtomicBool, AtomicU64, AtomicU16, Ordering },
         mpsc,
     },
 };
@@ -23,24 +21,143 @@ use crossterm::{
     },
 };
 
-use crate::proc::Field;
-
 type Err = Box<dyn std::error::Error>;
 type Result<T> = std::result::Result<T, Err>;
 
-#[derive(PartialEq, PartialOrd, Clone, Copy)]
-pub struct Rect{
-    w: u16, // j
-    h: u16, // i
+// Incapsulate for trust
+pub use rect::Rect;
+mod rect {
+
+    #[derive(PartialEq, PartialOrd, Clone, Copy)]
+    pub struct Rect{
+        w: u16, // j
+        h: u16, // i
+    }
+
+    impl Rect {
+        #[inline]
+        pub fn new(width: u16, height: u16) -> Self {
+            Rect{w: width, h: height}
+        }
+
+        #[inline]
+        pub fn term_size() -> Self {
+
+            let (width, height) = crossterm::terminal::size().unwrap();
+            Rect{w: width, h: height}
+        }
+
+        #[inline]
+        pub fn w(&self) -> u16 {
+            self.w
+        }
+
+        #[inline]
+        pub fn h(&self) -> u16 {
+            self.h
+        }
+
+        #[inline]
+        pub fn unwrap(&self) -> (u16, u16) {
+            (self.w, self.h)
+        }
+    }
+
 }
 
-pub struct App {
-    field: Mutex<Field>,
-    should_exit: AtomicBool,
-    pause: AtomicBool,
-    maxgen: AtomicU64,
-    upd_timeout: AtomicU64,
-    frames: AtomicU16,
+pub use app::App;
+mod app {
+
+    use crate::proc::Field;
+    use std::sync::{
+        Mutex,
+        atomic::{
+            AtomicU64,
+            AtomicU16,
+            AtomicBool,
+            Ordering,
+        }
+    };
+
+    pub struct App {
+        field: Mutex<Field>,
+        should_exit: AtomicBool,
+        pause: AtomicBool,
+        maxgen: AtomicU64,
+        upd_timeout: AtomicU64,
+        frames: AtomicU16,
+    }
+
+    impl App {
+
+        #[inline]
+        pub fn new(field: Field, maxgen: u64) -> Self {
+            App {
+                field: Mutex::new(field),
+                should_exit: false.into(),
+                pause: false.into(),
+                maxgen: maxgen.into(),
+                upd_timeout: 450.into(),
+                frames: 6.into(),
+            }
+        }
+
+        #[inline]
+        pub fn maxgen(&self) -> u64 {
+            self.maxgen.load(Ordering::Relaxed)
+        }
+
+        #[inline]
+        pub fn is_should_exit(&self) -> bool {
+            self.should_exit.load(Ordering::Relaxed)
+        }
+
+        #[inline]
+        pub fn is_paused(&self) -> bool {
+            self.pause.load(Ordering::Relaxed)
+        }
+
+        #[inline]
+        pub fn pause(&self) {
+            self.pause.store(self.pause.load(Ordering::SeqCst), Ordering::SeqCst)
+        }
+
+        #[inline]
+        pub fn upd_timeout(&self) -> u64 {
+            self.upd_timeout.load(Ordering::Relaxed)
+        }
+
+        #[inline]
+        pub fn need_frame(&self) {
+            self.frames.fetch_sub(1, Ordering::SeqCst);
+        }
+
+        #[inline]
+        pub fn add_frame(&self) {
+            self.frames.fetch_add(1, Ordering::SeqCst);
+        }
+
+        #[inline]
+        pub fn frames(&self) -> u16 {
+            self.frames.load(Ordering::SeqCst)
+        }
+
+        #[inline]
+        pub fn should_exit(&self) {
+            self.should_exit.store(true, Ordering::Relaxed)
+        }
+        
+        #[inline]
+        pub fn store_timeout(&self, t: u64) {
+            self.upd_timeout.store(t, Ordering::Relaxed)
+        }
+
+        #[inline]
+        pub fn field(&self) -> &Mutex<Field> {
+            &self.field
+        }
+    }
+
 }
 
 pub struct TimeoutIter {
@@ -87,85 +204,6 @@ impl TimeoutIter {
     }
 }
 
-impl Rect {
-    #[inline]
-    pub fn new(width: u16, height: u16) -> Self {
-        Rect{w: width, h: height}
-    }
-
-    #[inline]
-    pub fn term_size() -> Self {
-
-        let (width, height) = terminal::size().unwrap();
-        Rect{w: width, h: height}
-    }
-
-    #[inline]
-    pub fn w(&self) -> u16 {
-        self.w
-    }
-
-    #[inline]
-    pub fn h(&self) -> u16 {
-        self.h
-    }
-
-    #[inline]
-    pub fn unwrap(&self) -> (u16, u16) {
-        (self.w, self.h)
-    }
-}
-
-impl App {
-
-    #[inline]
-    pub fn new(field: Field, maxgen: u64) -> Self {
-        App {
-            field: Mutex::new(field),
-            should_exit: false.into(),
-            pause: false.into(),
-            maxgen: maxgen.into(),
-            upd_timeout: 450.into(),
-            frames: 6.into(),
-        }
-    }
-
-    #[inline]
-    pub fn maxgen(&self) -> u64 {
-        self.maxgen.load(Ordering::Relaxed)
-    }
-
-    #[inline]
-    pub fn should_exit(&self) -> bool {
-        self.should_exit.load(Ordering::Relaxed)
-    }
-
-    #[inline]
-    pub fn pause(&self) -> bool {
-        self.pause.load(Ordering::Relaxed)
-    }
-
-    #[inline]
-    pub fn upd_timeout(&self) -> u64 {
-        self.upd_timeout.load(Ordering::Relaxed)
-    }
-
-    #[inline]
-    pub fn need_frame(&self) {
-        self.frames.fetch_sub(1, Ordering::SeqCst);
-    }
-
-    #[inline]
-    pub fn add_frame(&self) {
-        self.frames.fetch_add(1, Ordering::SeqCst);
-    }
-
-    #[inline]
-    pub fn frames(&self) -> u16 {
-        self.frames.load(Ordering::SeqCst)
-    }
-}
-
 pub fn run(a: App) -> Result<()> {
 
     runup()?;
@@ -198,7 +236,7 @@ fn draw(a: App) -> Result<()> {
     let arc_keys = Arc::clone(&a);
 
     let _ = thread::Builder::new().name("Tick machine".into()).spawn(move || {
-        let mut field = arc_ticks.field.lock().unwrap();
+        let mut field = arc_ticks.field().lock().unwrap();
         let maxgen = arc_ticks.maxgen();
 
         for _ in 0..maxgen {
@@ -224,12 +262,13 @@ fn draw(a: App) -> Result<()> {
     });
 
     let mut gen = 0u64;
+    let mut f = !a.is_paused();
     while gen < a.maxgen() {
 
-        if a.should_exit() {
+        if a.is_should_exit() {
             break
         }
-        if !a.pause() {
+        if !a.is_paused() {
             gen += 1;
             a.need_frame();
             let field = rx.recv().unwrap();
@@ -246,11 +285,16 @@ fn draw(a: App) -> Result<()> {
                 }
                 print!("\n\r");
             }
+            f = true;
         } else {
+            if f {
+                f = false;
+                println!("Paused!\r")
+            }
             thread::sleep(Duration::from_millis(500));
         }
 
-        if a.should_exit() {
+        if a.is_should_exit() {
             break
         }
 
@@ -280,21 +324,17 @@ fn hotkeys(a: &Arc<App>, del: &mut TimeoutIter) -> Result<()> {
             if key.kind == KeyEventKind::Press {
                 if key.modifiers.contains(KeyModifiers::CONTROL) {
                     match key.code {
-                        KeyCode::Char('c') => a.should_exit.store(true, Ordering::Relaxed),
+                        KeyCode::Char('c') => a.should_exit(),
                         _ => {},
                     }
                 } else {
                     match key.code {
                         KeyCode::Char('p') => {
-                            let p = a.pause();
-                            a.pause.store(!p, Ordering::Relaxed);
-                            if p {
-                                println!("Paused!\r");
-                            }
+                            a.pause();
                         },
-                        KeyCode::Char('j') => a.upd_timeout.store(del.prev(), Ordering::Relaxed),
-                        KeyCode::Char('k') => a.upd_timeout.store(del.next(), Ordering::Relaxed),
-                        KeyCode::Char('q') => a.should_exit.store(true, Ordering::Relaxed),
+                        KeyCode::Char('j') => a.store_timeout(del.prev()),
+                        KeyCode::Char('k') => a.store_timeout(del.next()),
+                        KeyCode::Char('q') => a.should_exit(),
                         _ => {},
                     }
                 }
